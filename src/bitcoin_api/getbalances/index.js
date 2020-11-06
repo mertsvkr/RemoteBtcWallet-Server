bce78136428ca1357911bea4c0c984b7a9a353e3
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-06 14:56:59 +0300

    comment is added

--- src/bitcoin_api/getbalances/index.js
+++ src/bitcoin_api/getbalances/index.js
@@ -3,6 +3,7 @@ const Response = require("./response")
 const postRequest = require("../base/http/postRequest")
 
 exports.getBalances = async (walletName) => {
+    /**returns realBalance, pending balance and immature of the wallet. for more info go to the implementation of Response class*/
     var request = new Request({ method: "getbalance", wallet: walletName, params: [] })
     var htmlResponse = await postRequest(request)
     return new Response(htmlResponse)
