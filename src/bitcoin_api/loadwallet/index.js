0094eeb358fa2afdeeaf3cc9b3b5801798f7ab4c
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-06 14:49:04 +0300

    function name is changed from 'loadwallet' to 'loadWallet'

--- src/bitcoin_api/loadwallet/index.js
+++ src/bitcoin_api/loadwallet/index.js
@@ -2,8 +2,8 @@ const Request = require("./request")
 const Response = require("./response")
 const postRequest = require("../base/http/postRequest")
 
-exports.loadwallet = async (walletName) => {
+exports.loadWallet = async (walletName) => {
     const request = new Request({ method: "loadwallet", params: [walletName] })
     const httpResponse = await postRequest(request)
     return new Response(httpResponse)
-}
+} 
