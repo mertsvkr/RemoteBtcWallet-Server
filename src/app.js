fe69e59e38f8b94d2ac2c5e302017073d531e866
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-07 13:25:12 +0300

    bitcoin router is added to the server app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const authRouter = require('./routers/authentication')
+const bitcoinRouter = require("./routers/bitcoin")
 const port = process.env.PORT
 require('./db/db.js') // connects to the database
 require("./bitcoin_api") //  loads the old btc wallets to use them 
@@ -8,6 +9,7 @@ const app = express()
 
 app.use(express.json())
 app.use(authRouter)
+app.use(bitcoinRouter)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
