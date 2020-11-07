4b26a1996da025ebe3486e7b2ce1f2417580f60b
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-07 13:17:24 +0300

    controller and router are set for '/bitcoin/newtransaction' endpoint

--- src/controller/bitcoin.js
+++ src/controller/bitcoin.js
@@ -1,5 +1,7 @@
 const { getBalances } = require("../bitcoin_api/getbalances")
 const { getNewAddress } = require("../bitcoin_api/getnewaddress")
+const { sendToAddress } = require("../bitcoin_api/sendtoaddress")
+
 module.exports.getbalancesPostController = async (req, res) => {
     try {
         // get the balance from the bitcoin api by using the user's wallet name and return a response to the user
@@ -27,5 +29,20 @@ module.exports.getaddressPostController = async (req, res) => {
     } catch (e) {
         console.error(e)
         res.status(500).send({ success: false, error: "Unknown server error" })
+    }
+}
+
+module.exports.newtransactionPostController = async (req, res) => {
+    if (req.body.address && req.body.amount) {
+        const transactionResponse = await sendToAddress(req.user.wallet, req.body.address, req.body.amount)
+        if (transactionResponse.success) {
+            const balances = await getBalances(req.user.wallet)
+            res.status(200).send({ success: true, txid: transactionResponse.txid, balances: balances })
+        } else {
+            res.status(400).send({ success: false, error: transactionResponse.error })
+        }
+
+    } else {
+        res.status(400).send({ success: false, error: "Address and amount of BTC should be provided." })
     }
 }
