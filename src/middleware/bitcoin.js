8fcda0e7dd53644c30df7a1d7a95076b56511924
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-07 12:47:13 +0300

    middleware function for bitcoin operations is added

---
+++ src/middleware/bitcoin.js
@@ -0,0 +1,13 @@
+const User = require("../models/User")
+module.exports.validateId = async (req, res, next) => {
+    if (req.body.id) {
+        var user = User.findById(req.body.id)
+        if (user) {
+            next()
+        } else {
+            res.status(400).send({ success: false, error: "There is no such user with the given 'id' " })
+        }
+    } else {
+        res.status(400).send({ success: false, error: "Email and/or password are not provided" })
+    }
+}
