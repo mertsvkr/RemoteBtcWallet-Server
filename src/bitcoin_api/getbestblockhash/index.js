b943b7cc0c3b570ba1b4d325fa8b0af486054588
Mert Şavkar <48176417+mertsvkr@users.noreply.github.com>
2020-11-08 14:34:34 +0300

    comment is fixed

--- src/bitcoin_api/getbestblockhash/index.js
+++ src/bitcoin_api/getbestblockhash/index.js
@@ -3,7 +3,7 @@ const Response = require("./response")
 const postRequest = require("../base/http/postRequest")
 
 module.exports.getBestBlockHash = async () => {
-    /**returns realBalance, pending balance and immature of the wallet. for more info go to the implementation of Response class*/
+    /**returns the hash of the last block of the blockchain*/
     var request = new Request({ method: "getbestblockhash", params: [] })
     var htmlResponse = await postRequest(request)
     return new Response(htmlResponse)
